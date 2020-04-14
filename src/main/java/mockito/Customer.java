cc64c63121036cf78252c910530941ed999534ad
balaji38738 <bijjapwar123@gmail.com>
2020-04-14 15:52:42 +0530

    ADDED: Tested ClassToTest and Customer classes using dummy mockito object

---
+++ src/main/java/mockito/Customer.java
@@ -0,0 +1,11 @@
+package mockito;
+
+public class Customer {
+    private final String firstName;
+    private final String lastname;
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastname = lastName;
+    }
+}
